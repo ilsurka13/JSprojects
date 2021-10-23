e4647e4b8527e800777b8a1eafdbbda194d29371
Ilsur Kunafin <ilsurka13@aydnex.ru>
2021-10-23 23:36:53 +0300

    initial commit

---
+++ First/JS/script.js
@@ -0,0 +1,10 @@
+"use strict";
+//alert('Hello');
+
+//const result = confirm("are you here?");
+
+//console.log(result);
+
+const answers = [];
+answers[0] = prompt("Вам есть 18?", "");
+document.write(answers);

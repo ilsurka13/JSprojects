5fd8843d38cff169b16c1801044cb02855ac02a6
Ilsur Kunafin <ilsurka13@aydnex.ru>
2021-10-24 01:30:04 +0300

    First app

--- First/JS/script.js
+++ First/JS/script.js
@@ -1,9 +1,26 @@
 "use strict";
+
+const numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?');
+const b = +prompt('На сколько оцените его?','');
+const c = prompt('Один из последних просмотренных фильмов?');
+const d = +prompt('На сколько оцените его?','');
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
 //alert('Hello');
 
 //const result = confirm("are you here?");
 
 //console.log(result);
 
-const user = 'backdoor';
-console.log (`Привет`);
